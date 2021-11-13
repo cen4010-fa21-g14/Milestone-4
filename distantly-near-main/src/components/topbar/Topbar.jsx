98c5b926b32a730c4732f820d1968a0fa119e5d8
Ernesto Lopez <ernestolopez2018@fau.edu>
2021-11-12 21:14:13 -0500

    my 10th commit

--- distantly-near-main/src/components/topbar/Topbar.jsx
+++ distantly-near-main/src/components/topbar/Topbar.jsx
@@ -18,7 +18,7 @@ export default function Topbar() {
             </div>
             <div className="topbarCenter">
                 <div className="searchbar">
-                    <Search className="searchIcon"/>
+                    {/* <Search className="searchIcon"/> */}
                     <input placeholder="Search for friend, post or video" className="searchInput" />
                 </div>
             </div>
