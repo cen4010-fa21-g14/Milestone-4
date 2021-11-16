fe04552632430fd772a3c6353e9c13239d56c229
Ernesto Lopez <ernestolopez2018@fau.edu>
2021-11-15 19:15:05 -0500

    fixxing home

--- distantly-near-main/src/components/rightbar/Rightbar.jsx
+++ distantly-near-main/src/components/rightbar/Rightbar.jsx
@@ -58,24 +58,8 @@ export default function Rightbar({ user }) {
         <img className="rightbarAd" src={`${PF}ad.png`}  alt="" />
         <h4 className="rightbarTitle">Online Friends</h4>
         <ul className="rightbarFriendList">
-        {friends.map((friend) => (
-            <Link
-              to={"/profile/" + friend.username}
-              style={{ textDecoration: "none" }}
-            >
-              <div className="rightbarFollowing">
-                <img
-                  src={
-                    friend.profilePicture
-                      ? PF + friend.profilePicture
-                      : PF + "person/noAvatar.png"
-                  }
-                  alt=""
-                  className="rightbarFollowingImg"
-                />
-                <span className="rightbarFollowingName">{friend.username}</span>
-              </div>
-            </Link>
+          {Users.map((u) => (
+            <Online key={u.id} user={u} />
           ))}
         </ul>
       </>
